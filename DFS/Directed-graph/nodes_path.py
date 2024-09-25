fcf3ff118c1c42ce6af6226f8cc2e8fbbe734ee3
wlindor <47373982+wlindor@users.noreply.github.com>
2024-09-25 18:52:40 -0500

    making this OOP

--- DFS/Directed-graph/nodes_path.py
+++ DFS/Directed-graph/nodes_path.py
@@ -1,44 +1,55 @@
 # path to file: DFS/Directed-graph/nodes.py
 import collections
 
-given_edges = [(1,2), (1,3), (2,4), (4,5), (3,5), (5,6)]
-num_of_nodes = 6
+class DirectedGraph:
+    def __init__(self, edges, num_nodes) -> None:
+        self.edges = edges
+        self.num_nodes = num_nodes
+        self.adjancency_list = {}
 
-def graph_nodes(edges, number_of_nodes):
-    adjacency_list = {i: [] for i in range(1, number_of_nodes + 1)}
+    def set_adjancency_list(self) -> dict[int]:
+        self.adjancency_list = {i: [] for i in range(1, self.num_nodes + 1)}
 
-    for source_node,target_node in edges:
-        adjacency_list[source_node].append(target_node)
+        for source_node,target_node in self.edges:
+            self.adjancency_list[source_node].append(target_node)
 
-    return adjacency_list
+        return self.adjancency_list
 
-adjacency_list = graph_nodes(given_edges, num_of_nodes)
+    def dfs_recursion(self):
+        visited = set()
+        node = 1
 
-def can_visit_all_nodes(adj_list, num_nodes):
-    visited_dfs = set()
-    visited_stack = set()
+        def dfs(node):
+            if node not in visited:
+                visited.add(node)
+                for neighbor in self.adjancency_list[node]:
+                    dfs(neighbor)
 
-    def dfs(node):
-        if node not in visited_dfs:
-            visited_dfs.add(node)
-            for neighbor in adj_list[node]:
-                dfs(neighbor)
-        return len(visited_dfs) == num_nodes
-
-    def dfs_stack(node):
+        dfs(node)
+        return len(visited) == self.num_nodes
+    
+    def dfs_iteration(self):
         queue = collections.deque()
+        visited = set()
+        node = 1
         queue.append(node)
 
         while queue:
             cur_node = queue.pop()
-            if cur_node not in visited_stack:
-                visited_stack.add(cur_node)
-                for neighbor in adj_list[cur_node]:
+            if cur_node not in visited:
+                visited.add(cur_node)
+                for neighbor in self.adjancency_list[cur_node]:
                     queue.append(neighbor)
-        return len(visited_stack) == num_nodes
+        return len(visited) == self.num_nodes
+    
+given_edges = [(1,2), (1,3), (2,4), (4,5), (3,5), (5,6)]
+num_of_nodes = 6
+    
+graph1 = DirectedGraph(given_edges, num_of_nodes)
 
-    return (dfs(1))
-    return (dfs_stack(1))
+graph1.set_adjancency_list()
+print(graph1.dfs_recursion())
+print(graph1.dfs_iteration())
 
-print(can_visit_all_nodes(adjacency_list, num_of_nodes))
+
 
